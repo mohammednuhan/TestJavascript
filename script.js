4e74eb3249c8e0606247809c1d42fac7ccdcec13
SaqlenPasha <saqlenpasha47@gmail.com>
2025-01-28 13:19:43 +0530

    changes

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function addthreenumbers(a,b,c){
-    return a+b+c
-}
+// function addthreenumbers(a,b,c){
+//     return a+b+c
+// }
+// let c = addthreenumbers(3,3,7)
+// console.log(c)
 
+// let a = 3
+// console.log(a)
 
-let c = addthreenumbers(3,3,7)
+//  const b = 4        
+//  console.log(b)
 
-
-console.log(c)
+let a=null;
+let b=345;
+let c=12;
+let d="hi";
+let str=[1,2,3,4,5,6,7,8,"9","10"]
+console.log(a,b,c,d,str)
